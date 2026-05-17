63484049ffad1d0a8975d615fe5091e6d3795884
Alexey <alexey.anastasin@gmail.com>
2026-05-17 17:46:00 +0400

    feat: change text content on button click

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const orderButton = document.querySelector('.bth');
 orderButton.addEventListener('click', function() {
      alert ('Спасибо за заказ! Я свяжусь с вами.')
 });
+
+const emailButton = document.querySelector('.btn-secondary');
+const profileText = document.querySelector('.profile-text');
+
+emailButton.addEventListener('click', () => {
+    profileText.textContent = 'Моя почта: alexey@example.com';
+});
