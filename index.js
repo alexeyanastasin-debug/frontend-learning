688edf005433d43a776b7882a35d96c2c87efc33
Alexey <alexey.anastasin@gmail.com>
2026-05-17 17:50:18 +0400

    feat: toggle text content using if-else condition

--- index.js
+++ index.js
@@ -7,5 +7,13 @@ const emailButton = document.querySelector('.btn-secondary');
 const profileText = document.querySelector('.profile-text');
 
 emailButton.addEventListener('click', () => {
-    profileText.textContent = 'Моя почта: alexey@example.com';
+    if (isEmailShown === false) { 
+        profileText.textContent = 'Моя почта: alexey@example.com';
+        isEmailShown = true;
+    } else {
+         profileText.textContent = 'Цель: Стать Front-end разработчиком в 2026 году';
+         isEmailShown = false;
+    }
 });
+
+let isEmailShown = false;
